ea398b2eea0f2f10afcd934f3ee1020ad9f6ef9c
Rui Rocha <ruirocha1991@gmail.com>
2019-01-15 19:38:26 +0000

    install node

--- php/getPoint.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $URL ='http://localhost:9090/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=IS_Occurrence_Register%3Aoccurrences_polygon_lights&outputFormat=application%2Fjson&format_options=callback%3AgetJson&fbclid=IwAR2mAIr_eyv_DsKtas5P0VpLXOvgcKx0MzEjBRcyqlGSVGYKLIiPz1JeWn0';
-    echo file_get_contents($URL);
-?>
